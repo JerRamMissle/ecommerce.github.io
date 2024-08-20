d11d242f75246581fc5712861d0ae2d72cd8ecc8
JerRamMissle <114175041+JerRamMissle@users.noreply.github.com>
2024-08-19 20:09:24 -0400

    Updates

---
+++ precache-manifest.4362cdcbcea1ea9b444bc5861d1167e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a81c2c296a13b6a91c7b3631091bd5",
+    "url": "/ecommerce.github.io/index.html"
+  },
+  {
+    "revision": "7da6c14b189dfe0eacd7",
+    "url": "/ecommerce.github.io/static/js/2.5999e5ce.chunk.js"
+  },
+  {
+    "revision": "a712f5023b98f1ee6f879156125341ec",
+    "url": "/ecommerce.github.io/static/js/2.5999e5ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1af9adcc6779bfbf7a8",
+    "url": "/ecommerce.github.io/static/js/main.2587a800.chunk.js"
+  },
+  {
+    "revision": "5969c966ebbd4cfaecd6",
+    "url": "/ecommerce.github.io/static/js/runtime-main.bc8e0973.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/ecommerce.github.io/static/media/commerce.422fec10.png"
+  }
+]);
